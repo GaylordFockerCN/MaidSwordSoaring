174002eedf0f4714721b0cbc16b59e7933e0e554
p1nero <1070402601@qq.com>
2025-08-30 13:34:07 +0800

    防止无法右键交互

--- src/main/java/com/p1nero/maid_sword_soaring/entity/fly_sword/RideableSwordEntity.java
+++ src/main/java/com/p1nero/maid_sword_soaring/entity/fly_sword/RideableSwordEntity.java
@@ -1,13 +1,17 @@
 package com.p1nero.maid_sword_soaring.entity.fly_sword;
 
+import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import com.p1nero.maid_sword_soaring.MaidSwordSoaringMod;
 import com.p1nero.maid_sword_soaring.compat.ArmourersWorkshopCompat;
 import com.p1nero.maid_sword_soaring.entity.MaidSwordSoaringEntities;
 import com.p1nero.maid_sword_soaring.entity.ai.goal.SwordFollowTargetGoal;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
@@ -24,6 +28,14 @@ public class RideableSwordEntity extends SwordEntity {
         this.setPos(owner.position());
         this.setOwner(owner);
         this.setItemStack(owner.getMainHandItem());
+    }
+
+    @Override
+    protected InteractionResult mobInteract(Player pPlayer, InteractionHand pHand) {
+        if(!this.getPassengers().isEmpty() && this.getPassengers().get(0) instanceof EntityMaid maid) {
+            return maid.mobInteract(pPlayer, pHand);
+        }
+        return super.mobInteract(pPlayer, pHand);
     }
 
     @Override
